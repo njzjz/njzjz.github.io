a07ad8af146665520185b3e87abe0968c7728879
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2020-11-16 18:08:11 -0500

    Update jsdelivr_css.js

--- scripts/jsdelivr_css.js
+++ scripts/jsdelivr_css.js
@@ -1,20 +1,5 @@
-const http = require('http');
-hexo.extend.filter.register('after_generate', function(){
-  // obtain all.css
-  var data = hexo.route.get('all/all.css');
-  var cssdata = ""; 
-  var gitdata = "";
-  data.on('data', (chunk) => { cssdata += chunk; });
-  const request = http.get("https://raw.githubusercontent.com/njzjz/njzjz.github.io/master/css/main.css", function(res) {
-    res.on('data', (chunk) => { gitdata += chunk; });
-  });
-  // compare
-  if(cssdata == gitdata){
-    // get all html files
-    const routeList = hexo.route.list();
-    hexo.log.debug("same!")
-    //return Promise.all(routeList.filter(path => path.endsWith('.html')).map(path => {
-    //  const html = route.get(path);
-    //}));
+hexo.on('generateBefore', function () {
+  if(process.env.VERCEL_GITHUB_COMMIT_REF == "hexo") {
+    hexo.theme.config.css = "https://cdn.jsdelivr.net/gh/njzjz/njzjz.github.io@master/css";
   }
 });
