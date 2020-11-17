8dbd966a9de72a41a39d001c44672ab66794d00e
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2020-11-16 20:39:23 -0500

    Update jsdelivr_image.js

--- scripts/jsdelivr_image.js
+++ scripts/jsdelivr_image.js
@@ -1,4 +1,4 @@
 const { version } = require("hexo-theme-next/package.json");
 hexo.on('generateBefore', function () {
-  hexo.theme.config.css = `https://cdn.jsdelivr.net/npm/hexo-theme-next@${version}/source/images`;
+  hexo.theme.config.images = `https://cdn.jsdelivr.net/npm/hexo-theme-next@${version}/source/images`;
 });
