8857163d5a757112a9e9110466124c4d6f2033fb
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2020-11-17 17:26:29 -0500

    only remove when there is CSS_COMMIT

--- scripts/jsdelivr_css.js
+++ scripts/jsdelivr_css.js
@@ -1,9 +1,9 @@
-hexo.on('generateBefore', function () {
-  if(process.env.CSS_COMMIT) {
+if(process.env.CSS_COMMIT) {
+  hexo.on('generateBefore', function () {
     hexo.theme.config.css = `https://cdn.jsdelivr.net/gh/njzjz/njzjz.github.io@${process.env.CSS_COMMIT}`;
-  }
-});
+  });
 
-hexo.extend.filter.register('after_generate', () => {	
-  hexo.route.remove('css/main.css');
-});
+  hexo.extend.filter.register('after_generate', () => {	
+    hexo.route.remove('css/main.css');
+  });
+}
