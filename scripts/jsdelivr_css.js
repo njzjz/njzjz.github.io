4444e5ef078bed6a5f12916e4bab2f6fa6834a66
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2020-11-17 17:25:44 -0500

    remove main.css

--- scripts/jsdelivr_css.js
+++ scripts/jsdelivr_css.js
@@ -3,3 +3,7 @@ hexo.on('generateBefore', function () {
     hexo.theme.config.css = `https://cdn.jsdelivr.net/gh/njzjz/njzjz.github.io@${process.env.CSS_COMMIT}`;
   }
 });
+
+hexo.extend.filter.register('after_generate', () => {	
+  hexo.route.remove('css/main.css');
+});
