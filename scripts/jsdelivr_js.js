608203cb18843f2e674a8c9b3b086a234027b3e2
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2020-12-22 06:58:40 -0500

    chore: remove more js

--- scripts/jsdelivr_js.js
+++ scripts/jsdelivr_js.js
@@ -2,4 +2,6 @@ hexo.extend.filter.register('after_generate', () => {
     hexo.route.remove('js/schemes/muse.js');
     hexo.route.remove('js/next-boot.js');
     hexo.route.remove('js/utils.js');
-  });
+    hexo.route.remove('js/local-search.js');
+    hexo.route.remove('js/bookmark.js');
+});
