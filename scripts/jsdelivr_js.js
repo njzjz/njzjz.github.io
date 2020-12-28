f351428f99d573bfb5d2af2f0a327721d39cd9b7
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2020-12-28 07:17:49 -0500

    chore: use .min.js for jsdelivr js (#743)
    * chore: use .min.js for jsdelivr js
    
    * Update jsdelivr_js.js
    
    * Update jsdelivr_js.js
    
    * Update jsdelivr_js.js

--- scripts/jsdelivr_js.js
+++ scripts/jsdelivr_js.js
@@ -5,3 +5,20 @@ hexo.extend.filter.register('after_generate', () => {
     hexo.route.remove('js/local-search.js');
     hexo.route.remove('js/bookmark.js');
 });
+
+// .js -> .min.js
+function use_min_js(str, package, old_path, new_path){
+  var old_url = new RegExp(`//cdn.jsdelivr.net/npm/${package}@(([\\s\\S])*?)/${old_path}`.split('/').join('\\/'), "g");
+  var new_url = `//cdn.jsdelivr.net/npm/${package}@$1/${new_path}`;
+  return str.replace(old_url, new_url);
+}
+
+hexo.extend.filter.register('after_render:html', function(str, data){
+  str = use_min_js(str, 'hexo-theme-next', 'source/js/local-search.js', 'source/js/local-search.min.js');
+  str = use_min_js(str, 'hexo-theme-next', 'source/js/bookmark.js', 'source/js/bookmark.min.js');
+  str = use_min_js(str, 'hexo-theme-next', 'source/js/next-boot.js', 'source/js/next-boot.min.js');
+  str = use_min_js(str, 'hexo-theme-next', 'source/js/utils.js', 'source/js/utils.min.js');
+  str = use_min_js(str, 'nprogress', 'nprogress.js', 'nprogress.min.js');
+  str = use_min_js(str, 'nprogress', 'nprogress.css', 'nprogress.min.css');
+  return str;
+});
