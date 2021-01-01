11db3fd3c1ff1aea48d9b364351e071ef5524593
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-01-01 06:00:21 -0500

    add bsz

--- scripts/jsdelivr_js.js
+++ scripts/jsdelivr_js.js
@@ -6,11 +6,16 @@ hexo.extend.filter.register('after_generate', () => {
     hexo.route.remove('js/bookmark.js');
 });
 
+function replace_url(str, old_url, new_url){
+  var old_url = new RegExp(old_path.split('/').join('\\/'), "g");
+  return str.replace(old_url, new_url);
+}
+
 // .js -> .min.js
 function use_min_js(str, package, old_path, new_path){
-  var old_url = new RegExp(`//cdn.jsdelivr.net/npm/${package}@(([\\s\\S])*?)/${old_path}`.split('/').join('\\/'), "g");
+  var old_url = `//cdn.jsdelivr.net/npm/${package}@(([\\s\\S])*?)/${old_path}`;
   var new_url = `//cdn.jsdelivr.net/npm/${package}@$1/${new_path}`;
-  return str.replace(old_url, new_url);
+  return replace_url(str, old_url, new_url);
 }
 
 hexo.extend.filter.register('after_render:html', function(str, data){
@@ -20,5 +25,7 @@ hexo.extend.filter.register('after_render:html', function(str, data){
   str = use_min_js(str, 'hexo-theme-next', 'source/js/utils.js', 'source/js/utils.min.js');
   str = use_min_js(str, 'nprogress', 'nprogress.js', 'nprogress.min.js');
   str = use_min_js(str, 'nprogress', 'nprogress.css', 'nprogress.min.css');
+  // busuanzi
+  str = replace_url(str, '//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js', '//cdn.jsdelivr.net/gh/sukkaw/busuanzi@2.3/bsz.pure.mini.js');
   return str;
 });
