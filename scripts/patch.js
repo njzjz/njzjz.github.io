b0835d73e335121e4a39f3d50088cc9414643bfa
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-01-03 12:55:06 -0500

    chore: remove language patch

--- scripts/patch.js
+++ scripts/patch.js
@@ -1,7 +1,4 @@
-hexo.extend.filter.register('after_render:html', function(str, data){
-  // language
-  str = str.replace(/<html lang=(([\s\S])*?)>/, `<html lang="${data.page.lang}">`);
-  
+hexo.extend.filter.register('after_render:html', function(str, data){  
   // lazyload
   str = str.replace('class="site-author-image"', 'class="site-author-image" loading="lazy"');
   str = str.replace('alt="Creative Commons"', 'alt="Creative Commons" loading="lazy"');
