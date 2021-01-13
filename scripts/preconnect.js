7dafb3f801e832096482aae28ef293d3af335124
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-01-13 00:06:48 -0500

    chore: fix preconnect

--- scripts/preconnect.js
+++ scripts/preconnect.js
@@ -1,8 +1,2 @@
-const preconnect_domains = [
-    "https://comment.njzjz.win",
-    "https://www.google-analytics.com",
-];
-
-preconnect_domains.forEach(domain =>
-    hexo.extend.injector.register('head_end', `<link rel="preconnect" href="${domain}" crossorigin>`)
-);
+hexo.extend.injector.register('head_end', `<link rel="preconnect" href="https://comment.njzjz.win" crossorigin>`);
+hexo.extend.injector.register('head_end', `<link rel="preconnect" href="https://www.google-analytics.com">`);
