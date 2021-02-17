6af7c7db38eb9d24307065b0ae4946cd439049c0
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-02-17 16:06:31 -0500

    chore: fix github actions, use DO_NOT_REMOVE_CSS

--- scripts/jsdelivr_css.js
+++ scripts/jsdelivr_css.js
@@ -1,9 +1,11 @@
-if(process.env.CSS_COMMIT) {
+if (process.env.CSS_COMMIT) {
   hexo.on('generateBefore', function () {
     hexo.theme.config.css = `https://cdn.jsdelivr.net/gh/njzjz/njzjz.github.io@${process.env.CSS_COMMIT}`;
   });
 
-  hexo.extend.filter.register('after_generate', () => {	
-    hexo.route.remove('css/main.css');
-  });
+  if (!process.env.DO_NOT_REMOVE_CSS) {
+    hexo.extend.filter.register('after_generate', () => {
+      hexo.route.remove('css/main.css');
+    });
+  }
 }
