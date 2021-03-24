898b38561141ffded94399f0a8327adeb14970b7
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-03-23 23:36:36 -0400

    Delete preconnect.js

--- scripts/preconnect.js
+++
@@ -1,2 +0,0 @@
-hexo.extend.injector.register('head_end', `<link rel="dns-prefetch" href="https://comment.njzjz.win" crossorigin>`);
-hexo.extend.injector.register('head_end', `<link rel="dns-prefetch" href="https://www.google-analytics.com">`);
