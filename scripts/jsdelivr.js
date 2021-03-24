4bcf12f4676b0906ee119a036967cb36cddd807f
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-03-24 00:04:48 -0400

    chore: remove all routes from js directory (#895)

--- scripts/jsdelivr.js
+++ scripts/jsdelivr.js
@@ -3,27 +3,10 @@ const { npm_url, gh_url } = require("jsdelivr_url");
 //const { version } = require("hexo-theme-next/package.json");
 
 hexo.extend.filter.register('after_generate', () => {
-  // remove scripts
-  hexo.route.remove('js/schemes/muse.js');
-  hexo.route.remove('js/next-boot.js');
-  hexo.route.remove('js/utils.js');
-  hexo.route.remove('js/local-search.js');
-  hexo.route.remove('js/bookmark.js');
-
-  // remove images
-  hexo.route.remove('images/apple-touch-icon-next.png');
-  hexo.route.remove('images/avatar.gif');
-  hexo.route.remove('images/cc-by-nc-nd.svg');
-  hexo.route.remove('images/cc-by-nc-sa.svg');
-  hexo.route.remove('images/cc-by-nc.svg');
-  hexo.route.remove('images/cc-by-nd.svg');
-  hexo.route.remove('images/cc-by-sa.svg');
-  hexo.route.remove('images/cc-by.svg');
-  hexo.route.remove('images/cc-zero.svg');
-  hexo.route.remove('images/favicon-16x16-next.png');
-  hexo.route.remove('images/favicon-32x32-next.png');
-  hexo.route.remove('images/logo-algolia-nebula-blue-full.svg');
-  hexo.route.remove('images/logo.svg');
+  // remove all scripts from js and all images
+  hexo.route.list().filter(path => path.startsWith("js/") || path.startsWith("images/")).forEach(path => {
+    hexo.route.remove(path);
+  });
 });
 
 hexo.on('generateBefore', function () {
