3d226963392c7a6e8c8179ca590bd90230af4f7a
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-03-23 23:13:58 -0400

    test

--- scripts/preconnect.js
+++ scripts/preconnect.js
@@ -1,2 +1,8 @@
 hexo.extend.injector.register('head_end', `<link rel="dns-prefetch" href="https://comment.njzjz.win" crossorigin>`);
 hexo.extend.injector.register('head_end', `<link rel="dns-prefetch" href="https://www.google-analytics.com">`);
+
+hexo.extend.filter.register('theme_inject', injects => {
+
+  injects.head.raw('waline', `<link rel="dns-prefetch" href="1">`, {}, {});
+
+});
