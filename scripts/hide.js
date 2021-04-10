164ed2f791f76c2935cd847f4ebbd073bd94a3c3
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-04-10 17:25:50 -0400

    bugfix

--- scripts/hide.js
+++ scripts/hide.js
@@ -1,7 +1,7 @@
 const hidden_cat = ['ECNUChemistry', 'Internship'];
 
 hexo.extend.filter.register('before_post_render', function(data){
-  if (data.categories.filter(v => hidden_cat.includes(v))){
+  if (data.categories && data.categories.filter(v => hidden_cat.includes(v))){
     data.hidden = true;
   }
   return data;
