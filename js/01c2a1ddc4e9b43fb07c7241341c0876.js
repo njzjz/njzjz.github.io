11874544e97db2474c1c23006b4d79f10adf39e9
njzjz <jinzhe.zeng@rutgers.edu>
2021-04-19 18:18:44 +0000

    Deploying to master from @ njzjz/njzjz.github.io@746447f99b0f0bead65b6a3fa564ffbf494c9cdc 🚀

--- js/01c2a1ddc4e9b43fb07c7241341c0876.js
+++
@@ -1,1 +0,0 @@
-window.addEventListener("load",()=>{quicklink.listen({timeout:3e3,priority:!0,ignores:[i=>i.includes("#"),i=>"https://njzjz.win/zh-CN/archives/2018/page/2/"===i]})})
---
+++ js/ebcb9d1d13cd1ac831f5b152936784d1.js
@@ -0,0 +1,1 @@
+window.addEventListener("load",()=>{quicklink.listen({timeout:3e3,priority:!0,ignores:[i=>i.includes("#"),i=>"https://njzjz.win/archives/2019/05/"===i]})})
