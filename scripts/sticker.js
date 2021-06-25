5efbab1c9216613e7693b04f79cf3d1b50037a62
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-06-25 14:39:32 -0400

    update emoji option

--- scripts/sticker.js
+++ scripts/sticker.js
@@ -1,19 +1,8 @@
-const path = require("path");
-const fs = require("hexo-fs");
 const { npm_url } = require("jsdelivr_url");
 const { name, version } = require("sticker-heo/package.json");
 
 const idir = "Sticker-100";
-const dir = path.join(path.dirname(require.resolve(name)), idir);
-const icons = fs.listDirSync(dir).filter(fn => fn.endsWith(".png"))
-  .map(fn => fn.slice(0, -4));
 
 hexo.on('generateBefore', function () {
-  hexo.config.waline.emoji = [{
-    name: "Heo",
-    folder: npm_url(name, version, idir),
-    type: "png",
-    icon: "我看好你",
-    items: icons,
-  }];
+  hexo.config.waline.emoji = [npm_url(name, version, idir)];
 });
