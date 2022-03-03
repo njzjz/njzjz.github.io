d1f9756e1eec2dcd227d9d1c46c24d3e8a3d8a31
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-03-02 20:24:19 -0500

    disable jsdelivr for CSS

--- scripts/jsdelivr.js
+++ scripts/jsdelivr.js
@@ -17,7 +17,6 @@ hexo.on('generateBefore', function () {
   hexo.theme.config.vendors.nprogress_css = npm_url('nprogress', '0.2.0', 'nprogress.min.css');
   if (process.env.CSS_COMMIT) {
     hexo.config.assets_prefix = gh_url("njzjz", "njzjz.github.io", process.env.CSS_COMMIT, "");
-    hexo.theme.config.css = gh_url("njzjz", "njzjz.github.io", process.env.CSS_COMMIT, "css");
   }
 
   // icons
@@ -33,34 +32,3 @@ hexo.on('generateBefore', function () {
   };
 });
 
-hexo.extend.filter.register('after_generate', function (data) {
-  const hexo = this;
-  const reg = /<link(.*?) href="(.*?)\/main.css">/gi;
-  return new Promise((resolve, reject) => {
-    // read css and get md5
-    const html = hexo.route.get("css/main.css");
-    let cssContent = "";
-    html.on('data', (chunk) => (cssContent += chunk));
-    html.on('end', () => {
-      const css_hash = crypto.createHash('md5').update(cssContent).digest('hex');
-      const new_css_path = `css/${css_hash}.css`;
-      hexo.route.remove('css/main.css');
-      hexo.route.set(new_css_path, cssContent);
-      resolve(css_hash);
-    });
-  }).then((css_hash) => {
-    return Promise.all(hexo.route.list().filter(path => path.endsWith('.html')).map(path => {
-      return new Promise((resolve, reject) => {
-        const html = hexo.route.get(path);
-        let htmlContent = "";
-        html.on('data', (chunk) => (htmlContent += chunk));
-        html.on('end', () => {
-          hexo.route.set(path, htmlContent.replace(reg, function (str, p1, p2) {
-            return str.replace("main.css", `${css_hash}.css`);
-          }));
-        });
-        resolve();
-      });
-    }));
-  });
-});
