da687aaf129463acfacd7de9bf4ebeee7e1b453c
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-05-28 15:39:06 -0400

    fix: fix waline libUrl (#1311)
    see https://github.com/walinejs/waline/issues/2483

--- scripts/waline.js
+++ scripts/waline.js
@@ -2,6 +2,6 @@ const { version } = require("@waline/client/package.json");
 
 
 hexo.on('generateBefore', function () {
-  hexo.config.waline.libUrl = `https://cdnjs.cloudflare.com/ajax/libs/waline/${version}/waline.js`;
+  hexo.config.waline.libUrl = `https://cdnjs.cloudflare.com/ajax/libs/waline/${version}/waline.umd.js`;
   hexo.config.waline.cssUrl = `https://cdnjs.cloudflare.com/ajax/libs/waline/${version}/waline.min.css`;
 });
