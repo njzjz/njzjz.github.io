c7522d80e09e8bd05f6a9cecd32df6eef1548aca
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2025-02-18 22:26:37 +0800

    fix: migrate to jsdelivr due to cdnjs issues

--- scripts/waline.js
+++ scripts/waline.js
@@ -2,6 +2,6 @@ const { version } = require("@waline/client/package.json");
 
 
 hexo.on('generateBefore', function () {
-  hexo.config.waline.libUrl = `https://cdnjs.cloudflare.com/ajax/libs/waline/${version}/waline.umd.js`;
-  hexo.config.waline.cssUrl = `https://cdnjs.cloudflare.com/ajax/libs/waline/${version}/waline.min.css`;
+  hexo.config.waline.libUrl = `https://cdn.jsdelivr.net/npm/@waline/client@${version}/dist/waline.umd.min.js`;
+  hexo.config.waline.cssUrl = `https://cdn.jsdelivr.net/npm/@waline/client@${version}/waline.min.css`;
 });
